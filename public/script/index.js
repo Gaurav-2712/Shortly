bfc12ae7158c4f4afef3c926f1d7aaa11c84a6a1
Gaurav-2712 <gauravvadhavana2712@gmail.com>
2024-02-14 01:05:19 +0530

    small design changes

--- public/script/index.js
+++ public/script/index.js
@@ -2,3 +2,15 @@ import { GenerateNewURL } from './script.js';
 
 const urlForm = document.getElementById('generateUrl');
 urlForm.addEventListener('submit',GenerateNewURL);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuButton = document.getElementById("mobile-menu-btn");
+    const mobileMenu = document.getElementById("navbar-cta");
+
+    mobileMenuButton.addEventListener("click", function () {
+        mobileMenu.classList.toggle("hidden");
+        const expanded = mobileMenuButton.getAttribute("aria-expanded") === "true";
+        mobileMenuButton.setAttribute("aria-expanded", !expanded);
+    });
+});
